61e0f49b5fe873d6d571faa2be3f38e7ea1b5fca
Vlad Kovalenko <koval0310@gmail.com>
2021-02-10 11:24:18 +0200

    version 1.0.0

---
+++ pz1.9/script.js
@@ -0,0 +1,21 @@
+//halftriangle
+/*for (let i = 5; i >= 1; i--) {
+    let star = "";
+    for (let j = i; j <= 5; j++) {
+        star += "*";
+    }
+    console.log(star);
+}
+
+pyramid
+for (let i = 0; i < 5; i++) {
+    let star = '';
+    for (let j = 1; j < 5-i; j++) {
+        star = star + ' ';
+    }
+    for (let k = 1; k <= (2*i+1); k++) {
+        star = star + '*';
+    }
+    console.log(star);
+}*/
+
---
+++ pz1.9/second.js
@@ -0,0 +1,119 @@
+/*
+let i =0;
+do {
+    if(i==0){
+        console.log(`${i}-це нуль`)
+    }
+    else if(i%2==0){
+        console.log(`${i}-це парне`)
+    }
+    else if(i%2!=0){
+        console.log(`${i}-це непарне`)
+    }
+    i++;
+}while (i<=10);
+*/
+/*
+for (let i = 0; i < 10; i++)
+console.log(i);
+*/
+
+/*for (var result = 10000, counter = 0; result > 50; result = result / 2, counter++);
+console.log(result,counter);*/
+
+/*let tc= prompt("Температура за Цельсієм", "");
+let tf = (9/5)*tc + 32;
+alert("Температура за Фаренгейтом: " + tf);*/
+
+/*let num = Number(prompt("Число від 1 до 12",""));
+    switch(num){
+        case 1:
+            month="Зима-Січень";
+            alert("1" + month);
+            break;
+        case 2:
+            month="Зима-Лютий";
+            alert("" + month);
+            break;
+        case 3:
+            month="Зима-Березень";
+            alert("" + month);
+            break;
+        case 4:
+            month="Зима-Квітень";
+            alert("" + month);
+            break;
+        case 5:
+            month="Зима-Травень";
+            alert("" + month);
+            break;
+        case 6:
+            month="Зима-Червень";
+            alert("" + month);
+            break;
+        case 7:
+            month="Зима-Липень";
+            alert("" + month);
+            break;
+        case 8:
+            month="Зима-Серпень";
+            alert("" + month);
+            break;
+        case 9:
+            month="Зима-Вересень";
+            alert("" + month);
+            break;
+        case 10:
+            month="Зима-Жовтень";
+            alert("" + month);
+            break;
+        case 11:
+            month="Зима-Листопад";
+            alert("" + month);
+            break;
+        case 12:
+            month="Зима-Грудень";
+            alert("" + month);
+            break;
+    }*/
+
+/*let num = Number(prompt("Число від 1 до 7", ""));
+switch (num){
+    case 1:
+        alert("Понеділок");
+        break;
+    case 2:
+        alert("Вівторок");
+        break;
+    case 3:
+        alert("Середа");
+        break;
+    case 4:
+        alert("Четвер");
+        break;
+    case 5:
+        alert("П'ятниця");
+        break;
+    case 6:
+        alert("Субота");
+        break;
+    case 7:
+        alert("Неділя");
+        break;
+}*/
+let a = 100;
+
+nextPrime:
+    for(let i = 2; i <= a; i++) {
+        for (let j = 2; j < i; j++) {
+            if (i % j == 0) continue nextPrime;
+        }
+        console.log(i + " ");
+    }
+
+
+
+
+
+
+
