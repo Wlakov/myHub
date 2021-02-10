2db9d90541a5f26b93f7632b50dfb1a6824312fd
Vlad Kovalenko <koval0310@gmail.com>
2021-02-10 14:33:56 +0200

    update1

--- pz1.11/zavd1.js
+++ pz1.11/zavd1.js
@@ -1,4 +1,47 @@
+/*
 function dil(a){
     return a%60;
 }
-console.log(dil(122))
+console.log(dil(122))*/
+
+/*
+function per(n,a){
+    let perimetr;
+    return perimetr=n*a;
+}
+console.log(per(4,5))
+*/
+
+/*
+function sviyvivbir(n){
+    for(i=1;i<n;i++){
+     console.log(i);
+    }
+    function fizz(i){
+        if(i%3==0){
+            console.log(i);
+        }
+        return i;
+    }
+    return fizz();
+}
+console.log(sviyvivbir(10))*/
+
+/*
+function Calculate(a,b,c){
+    let average=(a+b+c)/3;
+    return average;
+}
+console.log(Calculate(2,4,6));*/
+
+/*function isDivisible(n,x,y){
+    let end;
+    if(n%x==0 && n%y==0){
+        end="Success";
+    }else{
+        end="Error";
+    }
+    return end;
+}
+console.log(isDivisible(12,5,4));*/
+
